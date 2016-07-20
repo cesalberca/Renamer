a8d331957f5d43635c2ccd4d9e8f1d2a6bd838d5
César Alberca <cesalberca@gmail.com>
2016-07-20 12:50:53 +0200

    Enforcing module pattern.

--- renamer.js
+++ renamer.js
@@ -1,54 +1,60 @@
 'use strict'
-var fs = require('fs-extra')
-var path = require('path')
+module.exports = (function () {
+  var fs = require('fs-extra')
+  var path = require('path')
 
-var rename = './rename/'
-var renamed = './renamed/'
-
-// Empties de directory
-fs.emptyDir(renamed, function (err) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(`${renamed} is now empty`)
-    renameFiles(rename)
+  function init (src, dest) {
+    // Empties de directory
+    fs.emptyDir(dest, function (err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(`${dest} is now empty`)
+        // Fires the main function
+        renameFiles(src, dest)
+      }
+    })
   }
-})
 
-// Rename the files inside rename and puts them on renamed
-function renameFiles (directory) {
-  fs.readdir(directory, function (err, files) {
-    if (err) {
-      console.log(err)
-    } else {
-      files.forEach(function (file) {
-        fs.copy(path.resolve(__dirname, directory, file), path.resolve(__dirname, renamed, getNewName(file)), function (err) {
-          if (err) {
-            throw err
-          } else {
-            console.log(`${file} - ${getNewName(file)}`)
-          }
+  // Rename the files inside src and puts them on dest
+  function renameFiles (src, dest) {
+    fs.readdir(src, function (err, files) {
+      if (err) {
+        console.log(err)
+      } else {
+        files.forEach(function (file) {
+          fs.copy(path.resolve(__dirname, src, file), path.resolve(__dirname, dest, getNewName(file)), function (err) {
+            if (err) {
+              throw err
+            } else {
+              console.log(`${file} - ${getNewName(file)}`)
+            }
+          })
         })
-      })
-    }
-  })
-}
+      }
+    })
+  }
 
-// Gets the final name, without spaces and in lower case
-function getNewName (filename) {
-  return `${removeExtension(replaceSpaces(filename))}.${getExtension(filename)}`
-}
+  // Gets the final name, without spaces and in lower case
+  function getNewName (filename) {
+    return `${removeExtension(replaceSpaces(filename))}.${getExtension(filename)}`
+  }
 
-// Gets extension given the filename
-function getExtension (filename) {
-  var ext = path.extname(filename || '').split('.')
-  return ext[ext.length - 1]
-}
+  // Gets extension given the filename
+  function getExtension (filename) {
+    var ext = path.extname(filename || '').split('.')
+    return ext[ext.length - 1]
+  }
 
-function removeExtension (filename) {
-  return filename.replace(/\.[^/.]+$/, '')
-}
+  function removeExtension (filename) {
+    return filename.replace(/\.[^/.]+$/, '')
+  }
 
-function replaceSpaces (string) {
-  return string.replace(/ /g, '-').toLowerCase()
-}
+  function replaceSpaces (string) {
+    return string.replace(/ /g, '-').toLowerCase()
+  }
+
+  return {
+    init: init
+  }
+})()
