6bc69ab46e8c1702ebe0939cb62276af5fe9c2b5
César Alberca <cesalberca@gmail.com>
2016-07-21 12:04:38 +0200

    Replaces now special chars

--- renamer.js
+++ renamer.js
@@ -45,16 +45,26 @@ module.exports = (function () {
     })
   }
 
-  // Gets the final name, without spaces and in lower case
+  // Gets the final name, without spaces, without special characters and in lower case
   function getNewName (string) {
-    return replaceSpaces(removeExtraSpaces(string)).toLowerCase()
+    var withoutSpecialCharacters = replaceSpecialCharacters(replaceSpanishN(string))
+    return replaceSpacesWithDashes(removeExtraSpaces(withoutSpecialCharacters)).toLowerCase()
+  }
+
+  function replaceSpecialCharacters (string) {
+    var regex = /[^a-zA-Z0-9_\-\.\s]/g
+    return string.replace(regex, '')
+  }
+
+  function replaceSpanishN (string) {
+    return string.replace(/[ñ]/g, 'n')
   }
 
   function removeExtraSpaces (string) {
     return string.replace(/\s{2,}/g, ' ')
   }
 
-  function replaceSpaces (string) {
+  function replaceSpacesWithDashes (string) {
     return string.replace(/ /g, '-')
   }
 
